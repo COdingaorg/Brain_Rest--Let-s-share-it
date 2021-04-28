4a219d12ed1dc84aeee712fb73bd644622ec3fba
Fai <chepngetichfaith46@gmail.com>
2021-04-28 14:42:09 +0300

    Add The Toggle effect on the community top stories and images

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 
 //User Logic
 $(document).ready(function (event) {
+  // user story one.
   $('#celebrateHealth, #link1').click(function (event) {
     event.preventDefault()
 
@@ -9,7 +10,33 @@ $(document).ready(function (event) {
   })
   $('#closeStory').click(function () {
     $('#userStoriesPage').hide()
+
   })
+  // user story 2.
+  $('#coupleStress, #link2').click(function(event){
+    event.preventDefault()
+    $('#story2').show()
+  })
+  $('#closeStory2').click(function () {
+    $('#story2').hide()
+  })
+  // user story 3
+  $('#mindCleansing, #link3').click(function(event){
+    event.preventDefault()
+    $('#story3').show()
+  })
+  $('#closeStory3').click(function () {
+    $('#story3').hide()
+  })
+  // user story 4
+  $('#crashing, #link4').click(function(event){
+    event.preventDefault()
+    $('#story4').show()
+  })
+  $('#closeStory4').click(function () {
+    $('#story4').hide()
+  })
+  // signup page.
   $('#signUpPage').click(function(event){
     event.preventDefault()
     $('#signupCont').show()
